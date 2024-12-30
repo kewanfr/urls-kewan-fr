90f065e0498a1453061257b63f5f8aa9fa520935
Kéwan B <mail@kewan.fr>
2024-12-31 00:15:22 +0100

    update

--- login.php
+++ login.php
@@ -105,7 +105,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <footer class="bg-white shadow mt-10">
         <div class="container mx-auto px-4 py-6 text-center text-gray-500">
-        <?= date('Y') ?> Urls Kéwan.fr - Développé par <a href="https://kewan.fr" class="text-blue-600 hover:text-blue-500">Kéwan</a>
+        <?= date('Y') ?> Urls Kéwan.fr - Développé par <a href="https://kewan.fr" target="_blank" class="text-blue-600 hover:text-blue-500">Kéwan B</a>
         </div>
     </footer>
 </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./**/*.php",
-  "./**/*.html"
-],
+    "./**/*.php",
+    "./**/*.html"
+  ],
   theme: {
     extend: {},
   },
